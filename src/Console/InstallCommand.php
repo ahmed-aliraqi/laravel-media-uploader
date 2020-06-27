3f790f8e0d2cb3dd79cf5f9989c4c0c9dbabd939
Ahmed Fathy <aliraqi.dev@gmail.com>
2020-06-27 20:53:54 +0200

    formatting

--- src/Console/InstallCommand.php
+++ src/Console/InstallCommand.php
@@ -3,7 +3,6 @@
 namespace AhmedAliraqi\LaravelMediaUploader\Console;
 
 use Illuminate\Console\Command;
-use AhmedAliraqi\LaravelMediaUploader\Entities\TemporaryFile;
 
 class InstallCommand extends Command
 {
