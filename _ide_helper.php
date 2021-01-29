40154df6438355f649f00ef7160b32cbb3cff74c
Ahmed Fathy <aliraqi.dev@gmail.com>
2021-01-29 23:03:49 +0200

    add _ide_helper.php

---
+++ _ide_helper.php
@@ -0,0 +1,21 @@
+<?php
+// @formatter:off
+
+/**
+ * A helper file for Laravel, to provide autocomplete information to your IDE
+ **
+ * @author Ahmed Fathy <aliraqi.dev@gmail.com>
+ *
+ * @method static image
+ */
+
+/**
+ * Class BsForm
+ * @method static \AhmedAliraqi\LaravelMediaUploader\Forms\Components\ImageComponent image($name = null)
+ * @method static \AhmedAliraqi\LaravelMediaUploader\Forms\Components\AudioComponent audio($name = null)
+ * @method static \AhmedAliraqi\LaravelMediaUploader\Forms\Components\VideoComponent video($name = null)
+ * @method static \AhmedAliraqi\LaravelMediaUploader\Forms\Components\MediaComponent media($name = null)
+ */
+class BsForm extends \Laraeast\LaravelBootstrapForms\Facades\BsForm {
+
+}
