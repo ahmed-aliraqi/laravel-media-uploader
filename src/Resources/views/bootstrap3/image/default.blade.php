c9677145543d2413ebec0e96694e866c2ccc815a
Ahmed Fathy <aliraqi.dev@gmail.com>
2023-01-05 04:17:51 +0200

    add name parameter

--- src/Resources/views/bootstrap3/image/default.blade.php
+++ src/Resources/views/bootstrap3/image/default.blade.php
@@ -1,5 +1,6 @@
 @php($files = is_array($files) ? json_encode($files) : $files)
 <file-uploader :media="{{ $files ?? '[]' }}"
+               {{ $name ? ':name="'.$name.'"' : '' }}
                :max="{{ $max ?? 1 }}"
                collection="{{ $collection }}"
                :tokens="{{ json_encode(old('media', [])) }}"
